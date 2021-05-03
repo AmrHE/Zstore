12ed7b09d1b40c96d905b5a678902e6f56ce61b0
AmrHE <amr.hassan.emam@gmail.com>
2021-05-03 23:55:09 +0200

    add styled-components

--- Zstore/frontend/components/Header.js
+++ Zstore/frontend/components/Header.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
 
 const Header = () => (
-  <header>
+  <HeaderStyles>
     <div className="bar">
-      <Link href="/">ZStore</Link>
+      <Logo>
+        <Link href="/">ZStore</Link>
+      </Logo>
     </div>
     <div className="sub-bar">
       <p>Search Bar</p>
     </div>
     <Nav />
-  </header>
+  </HeaderStyles>
 );
 
 export default Header;
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
