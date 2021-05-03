8a51e08899ccf797f2579e5f2fcef914392ca1f9
AmrHE <amr.hassan.emam@gmail.com>
2021-05-03 23:56:13 +0200

    edit href's

--- Zstore/frontend/components/Nav.js
+++ Zstore/frontend/components/Nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Nav = () => (
   <nav>
-    <Link href="products">Products</Link>
+    <Link href="/products">Products</Link>
     <Link href="/sell">Sell</Link>
     <Link href="/orders">Orders</Link>
     <Link href="/account">Account</Link>
