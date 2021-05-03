44211e0cf2368a76d03f57cc7327fd3a23b62c34
AmrHE <amr.hassan.emam@gmail.com>
2021-05-03 23:57:44 +0200

    add styled-components

--- Zstore/frontend/components/Page.js
+++ Zstore/frontend/components/Page.js
@@ -1,13 +1,69 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
 const Page = ({ children }) => (
-  <>
+  <div>
+    <GlobalStyles />
     <Header>
-      <h1>I'm The Page Component</h1>
-      {children}
+      <InnerStyles>
+        <h1>I'm The Page Component</h1>
+        {children}
+      </InnerStyles>
     </Header>
-  </>
+  </div>
 );
 
 export default Page;
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+  font-family: 'radnika_next';
+  src: url('/static/radnikanext-medium-webfont.woff2')
+  format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+  html{
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3a3a3a;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(----lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+    box-sizing: border-box;
+
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body{
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif,--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0px;
+    margin: 0px;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+
+  a{
+    text-decoration: none;
+    color: var(--black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  button {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif,--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
