b0dc4b7d1a9fca09d7ec88f347fab29c156386e7
AmrHE <amr.hassan.emam@gmail.com>
2021-05-04 05:21:03 +0200

    add Apollo Client

--- Zstore/frontend/pages/_app.js
+++ Zstore/frontend/pages/_app.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/client';
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Page';
-
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-const MyApp = ({ Component, pageProps }) => (
-  <Page>
-    <Component {...pageProps} />
-  </Page>
-);
+const MyApp = ({ Component, pageProps, apollo }) => {
+  console.log(apollo);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+};
 
-export default MyApp;
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  const pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
