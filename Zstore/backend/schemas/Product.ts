37f3f0f3cdbacc6a0f60dca42f91b6e4b128bb89
AmrHE <amr.hassan.emam@gmail.com>
2021-05-04 05:16:48 +0200

    add Product Schema

---
+++ Zstore/backend/schemas/Product.ts
@@ -0,0 +1,39 @@
+import { integer, relationship, select, text } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+
+export const Product = list({
+  // Todo
+  // Access:
+  fields: {
+    name: text({ isRequired: true }),
+    description: text({
+      isRequired: true,
+      ui: {
+        displayMode: 'textarea',
+      },
+    }),
+    // TODO: add photo
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
+    status: select({
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'UnAvailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+    price: integer(),
+  },
+});
