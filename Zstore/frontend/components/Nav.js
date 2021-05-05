c927523fa1a2cc583f2c096b46faa827ab933f58
AmrHE <amr.hassan.emam@gmail.com>
2021-05-05 06:06:54 +0200

    add NavStyles component

--- Zstore/frontend/components/Nav.js
+++ Zstore/frontend/components/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
-  <nav>
+  <NavStyles>
     <Link href="/products">Products</Link>
     <Link href="/sell">Sell</Link>
     <Link href="/orders">Orders</Link>
     <Link href="/account">Account</Link>
-  </nav>
+  </NavStyles>
 );
 
 export default Nav;
