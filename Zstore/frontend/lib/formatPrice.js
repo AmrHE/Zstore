4732048aad05b75038fe91c139cccc7c9f1fb48e
AmrHE <amr.hassan.emam@gmail.com>
2021-05-05 06:10:30 +0200

    create formatPrice func

---
+++ Zstore/frontend/lib/formatPrice.js
@@ -0,0 +1,18 @@
+const formatPrice = (amount = 0) => {
+  const options = {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  };
+
+  // Check if it's a clean dollar amount or not
+  if (amount % 100 === 0) {
+    options.minimumFractionDigits = 0;
+  }
+
+  const formatter = Intl.NumberFormat('en-US', options);
+
+  return formatter.format(amount / 100);
+};
+
+export default formatPrice;
