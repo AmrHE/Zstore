c44b2feb912459845a4c2d35f22667d65cab4e61
AmrHE <amr.hassan.emam@gmail.com>
2021-05-09 08:21:35 +0200

    create UpdateProduct component

---
+++ Zstore/frontend/components/UpdateProduct.js
@@ -0,0 +1,121 @@
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
+import Form from './styles/Form';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      data: { name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+const UpdateProduct = ({ id }) => {
+  // 1. get the existing product data
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+  console.log(data);
+
+  // 2. get the mutation to update the product
+  const [
+    updateProduct,
+    { data: updateData, error: updateError, loading: updateLoading },
+  ] = useMutation(UPDATE_PRODUCT_MUTATION);
+
+  // 3. create state for the form inputs
+  const { inputs, handleChange, clearForm, resetForm } = useForm(data?.Product);
+  console.log(inputs);
+  if (loading) return <p>Loading...</p>;
+
+  // 4. form that handle the updates UI
+
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const res = await updateProduct({
+          variables: {
+            id,
+            name: inputs.name,
+            description: inputs.description,
+            price: inputs.price,
+          },
+        }).catch(console.error);
+        console.log({ res });
+        // Todo: HandleSubmit
+        // // Submit the input feilds to the backend
+        // const res = await createProduct();
+        // clearForm();
+        // // Redirect to the created product's page!
+        // Router.push({
+        //   pathname: `/product/${res.data.createProduct.id}`,
+        // });
+      }}
+    >
+      <DisplayError error={error || updateError} />
+      <fieldset disabled={updateLoading} aria-busy={updateLoading}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Update Product</button>
+      </fieldset>
+    </Form>
+  );
+};
+
+export default UpdateProduct;
