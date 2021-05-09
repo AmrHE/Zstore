7bbb68217de193e59d5fcdbb3703b67166f20f3b
AmrHE <amr.hassan.emam@gmail.com>
2021-05-09 08:17:02 +0200

    add Create_Product_Mutation

--- Zstore/frontend/components/CreateProduct.js
+++ Zstore/frontend/components/CreateProduct.js
@@ -1,5 +1,35 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # Which variable are getting passed in? and what types are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 const CreateProduct = () => {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -9,13 +39,30 @@ const CreateProduct = () => {
     image: '',
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(inputs);
-  };
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
+    }
+  );
+  console.log(data);
+
   return (
-    <Form onSubmit={handleSubmit}>
-      <fieldset aria-busy>
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        // Submit the input feilds to the backend
+        const res = await createProduct();
+        clearForm();
+        // Redirect to the created product's page!
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`,
+        });
+      }}
+    >
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="name">
           Name
           <input
