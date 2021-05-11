6c99474e0d87ded8efcfcd66116b07ffbd545d74
AmrHE <amr.hassan.emam@gmail.com>
2021-05-11 07:52:58 +0200

    update gql query

--- Zstore/frontend/components/Products.js
+++ Zstore/frontend/components/Products.js
@@ -2,11 +2,12 @@ import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import React from 'react';
 import styled from 'styled-components';
+import { perPage } from '../config';
 import Product from './Product';
 
 export const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       price
@@ -21,8 +22,13 @@ export const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
-const Products = () => {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+const Products = ({ page }) => {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
   return (
