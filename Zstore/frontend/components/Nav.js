def939bdd92db509b6faae170ebeeac56ef20bb0
AmrHE <amr.hassan.emam@gmail.com>
2021-05-18 23:00:30 +0200

    add user/useUser

--- Zstore/frontend/components/Nav.js
+++ Zstore/frontend/components/Nav.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import { useUser } from './User';
 
-const Nav = () => (
-  <NavStyles>
-    <Link href="/products">Products</Link>
-    <Link href="/sell">Sell</Link>
-    <Link href="/orders">Orders</Link>
-    <Link href="/account">Account</Link>
-  </NavStyles>
-);
-
+const Nav = () => {
+  const user = useUser();
+  console.log(user);
+  return (
+    <NavStyles>
+      <Link href="/products">Products</Link>
+      {user && (
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+        </>
+      )}
+      {!user && (
+        <>
+          <Link href="/signin">Sign In</Link>
+        </>
+      )}
+    </NavStyles>
+  );
+};
 export default Nav;
