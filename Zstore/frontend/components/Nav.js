d527c8d379cbc72f8d5c044a69f2f03e813229ac
AmrHE <amr.hassan.emam@gmail.com>
2021-05-19 23:04:48 +0200

    import SignOut component

--- Zstore/frontend/components/Nav.js
+++ Zstore/frontend/components/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
+import SignOut from './SignOut';
 
 const Nav = () => {
   const user = useUser();
@@ -14,6 +15,7 @@ const Nav = () => {
           <Link href="/sell">Sell</Link>
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
+          <SignOut />
         </>
       )}
       {!user && (
