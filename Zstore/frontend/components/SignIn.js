73144bc975bb8b4e3664776f2d9b40a4ff44de06
AmrHE <amr.hassan.emam@gmail.com>
2021-05-19 23:01:52 +0200

    create SignIn component

---
+++ Zstore/frontend/components/SignIn.js
@@ -0,0 +1,88 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
+import Form from './styles/Form';
+import { CURRENT_USER_QUERY } from './User';
+
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    authenticateUserWithPassword(email: $email, password: $password) {
+      ... on UserAuthenticationWithPasswordSuccess {
+        item {
+          id
+          email
+          name
+        }
+      }
+      ... on UserAuthenticationWithPasswordFailure {
+        code
+        message
+      }
+    }
+  }
+`;
+
+const SignIn = () => {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+  });
+
+  const [signin, { loading, data }] = useMutation(SIGNIN_MUTATION, {
+    variables: inputs,
+    // Refetch the currently logged in user
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log({ inputs });
+    const res = await signin();
+    console.log({ res });
+    resetForm();
+    // Send the email and password to the GraphQL API
+  };
+
+  const error =
+    data?.authenticateUserWithPassword.__typename ===
+    'UserAuthenticationWithPasswordFailure'
+      ? data?.authenticateUserWithPassword
+      : undefined;
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Into Your Account</h2>
+      <DisplayError error={error} />
+      <fieldset>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In!</button>
+      </fieldset>
+    </Form>
+  );
+};
+
+export default SignIn;
