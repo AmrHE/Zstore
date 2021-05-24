5d8a94b05e4238cd27ddb317c9fc23b8b4e24d67
AmrHE <amr.hassan.emam@gmail.com>
2021-05-24 23:34:30 +0200

    edit Cart

--- Zstore/frontend/components/Cart.js
+++ Zstore/frontend/components/Cart.js
@@ -1,22 +1,33 @@
 import CartStyles from './styles/CartStyles';
+import CloseButton from './styles/CloseButton';
 import Supreme from './styles/Supreme';
 import { useUser } from './User';
 import CartItem from './CartItem';
+import calcTotalPrice from '../lib/calcTotalPrice';
+import formatPrice from '../lib/formatPrice';
+import { useCart } from '../lib/cartState';
 
 const Cart = () => {
   const user = useUser();
-  console.log({ user });
+  const { cartOpen, closeCart } = useCart();
+
   if (!user) return null;
   return (
-    <CartStyles open>
+    <CartStyles open={cartOpen}>
       <header>
         <Supreme>{user.name}'s Cart</Supreme>
+        <CloseButton type="button" onClick={closeCart}>
+          &times;
+        </CloseButton>
       </header>
       <ul>
         {user.cart.map((cartItem) => (
           <CartItem key={cartItem.id} cartItem={cartItem} />
         ))}
       </ul>
+      <footer>
+        <p>{formatPrice(calcTotalPrice(user.cart))}</p>
+      </footer>
     </CartStyles>
   );
 };
