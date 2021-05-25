a78961acbb64a8d348c11790265018de94203544
AmrHE <amr.hassan.emam@gmail.com>
2021-05-25 23:34:49 +0200

    extend graphql schema

--- Zstore/backend/keystone.ts
+++ Zstore/backend/keystone.ts
@@ -11,6 +11,7 @@ import { ProductImage } from './schemas/ProductImage';
 import { CartItem } from './schemas/CartItem';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
+import { extendGraphqlSchema } from './mutations';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -59,6 +60,7 @@ export default withAuth(
       ProductImage,
       CartItem,
     }),
+    extendGraphqlSchema,
     ui: {
       // show the UI only for people who pass this test
       isAccessAllowed: ({ session }) =>
