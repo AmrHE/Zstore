d1eff3413714a8ede89d631ae50703a7a3049309
AmrHE <amr.hassan.emam@gmail.com>
2021-05-26 22:47:43 +0200

    import CartCount

--- Zstore/frontend/components/Nav.js
+++ Zstore/frontend/components/Nav.js
@@ -4,6 +4,7 @@ import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
 import SignOut from './SignOut';
 import { useCart } from '../lib/cartState';
+import CartCount from './CartCount';
 
 const Nav = () => {
   const user = useUser();
@@ -19,6 +20,12 @@ const Nav = () => {
           <SignOut />
           <button type="button" onClick={openCart}>
             My Cart
+            <CartCount
+              count={user.cart.reduce(
+                (tally, cartItem) => tally + cartItem.quantity,
+                0
+              )}
+            />
           </button>
         </>
       )}
