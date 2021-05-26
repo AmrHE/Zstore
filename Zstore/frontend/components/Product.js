5258bef1e1c41a3471309074ff1268a1ce161133
AmrHE <amr.hassan.emam@gmail.com>
2021-05-26 22:48:20 +0200

    import AddToCart

--- Zstore/frontend/components/Product.js
+++ Zstore/frontend/components/Product.js
@@ -5,6 +5,7 @@ import Title from './styles/Title';
 import PriceTag from './styles/PriceTag';
 import formatPrice from '../lib/formatPrice';
 import DeleteProduct from './DeleteProduct';
+import AddToCart from './AddToCart';
 
 const Product = ({ product }) => (
   <div>
@@ -29,6 +30,7 @@ const Product = ({ product }) => (
         >
           Edit ✏️
         </Link>
+        <AddToCart id={product.id} />
         <DeleteProduct id={product.id}>Delete</DeleteProduct>
       </div>
     </ItemStyles>
