50789e8670fe938c3612a595782be72c043e1bb9
AmrHE <amr.hassan.emam@gmail.com>
2021-08-24 00:10:50 +0200

    add access/orders/products

--- Zstore/backend/schemas/User.ts
+++ Zstore/backend/schemas/User.ts
@@ -1,9 +1,21 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
+import { permissions, rules } from '../access';
 
 export const User = list({
-  // access:
-  // ui
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    // user Cannot delete himself
+    // Only users with permissions can delete themselves or can delete users
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    // Hide the bacnend UI from regular users
+    hideCreate: (args) => !permissions.canManageUsers,
+    hideDelete: (args) => !permissions.canManageUsers,
+  },
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
@@ -16,6 +28,18 @@ export const User = list({
         itemView: { fieldMode: 'read' },
       },
     }),
-    // TODO: add roles, and orders
+    orders: relationship({ ref: 'Order.user', many: true }),
+    role: relationship({
+      ref: 'Role.assignedTo',
+      // Limit the access of creating a new role
+      access: {
+        create: permissions.canManageUsers,
+        update: permissions.canManageUsers,
+      },
+    }),
+    products: relationship({
+      ref: 'Product.user',
+      many: true,
+    }),
   },
 });
