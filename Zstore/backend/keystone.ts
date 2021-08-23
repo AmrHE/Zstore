a4eac238f5472d3cec0528fbe5b3898187540668
AmrHE <amr.hassan.emam@gmail.com>
2021-08-24 00:11:26 +0200

    import new schemas

--- Zstore/backend/keystone.ts
+++ Zstore/backend/keystone.ts
@@ -9,9 +9,13 @@ import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import { CartItem } from './schemas/CartItem';
+import { Order } from './schemas/Order';
+import { Role } from './schemas/Role';
+import { OrderItem } from './schemas/OrderItem';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
 import { extendGraphqlSchema } from './mutations';
+import { permissionsList } from './schemas/fields';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -59,6 +63,9 @@ export default withAuth(
       Product,
       ProductImage,
       CartItem,
+      OrderItem,
+      Order,
+      Role,
     }),
     extendGraphqlSchema,
     ui: {
@@ -69,7 +76,7 @@ export default withAuth(
     },
     session: withItemData(statelessSessions(sessionConfig), {
       // GraphQl Query
-      User: 'id name email',
+      User: `id name email role { ${permissionsList.join()} }`,
     }),
   })
 );
