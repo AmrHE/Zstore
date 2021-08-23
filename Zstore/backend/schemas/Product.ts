c025e51e2867d4da11bb6789920355bd39df443a
AmrHE <amr.hassan.emam@gmail.com>
2021-08-24 00:09:03 +0200

    add access/user

--- Zstore/backend/schemas/Product.ts
+++ Zstore/backend/schemas/Product.ts
@@ -1,9 +1,15 @@
 import { integer, relationship, select, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { isSignedIn, rules } from '../access';
 
 export const Product = list({
-  // Todo
-  // Access:
+  access: {
+    create: isSignedIn,
+    read: rules.canReadProducts,
+    update: rules.canManageProducts,
+    delete: rules.canManageProducts,
+  },
+
   fields: {
     name: text({ isRequired: true }),
     description: text({
@@ -12,7 +18,6 @@ export const Product = list({
         displayMode: 'textarea',
       },
     }),
-    // TODO: add photo
     photo: relationship({
       ref: 'ProductImage.product',
       ui: {
@@ -35,5 +40,11 @@ export const Product = list({
       },
     }),
     price: integer(),
+    user: relationship({
+      ref: 'User.products',
+      defaultValue: ({ context }) => ({
+        connect: { id: context.session.itemId },
+      }),
+    }),
   },
 });
